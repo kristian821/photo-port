81b51c665590b38c2f2a8f7710f8d810c008e364
Kristian <56667566+kristian821@users.noreply.github.com>
2021-06-15 19:16:40 -0700

    added contact form

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -107,7 +107,7 @@ function PhotoList({ category }) {
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
-                    src={require(`../../assets/small/${category}/${i}.jpg`)}
+                    src={import(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className="img-thumbnail mx-1"
                     key={image.name}
